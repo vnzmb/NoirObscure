7a76d032b69c5339bb42a83d2c099782aa797af8
Ian Shepherd <me@ianshepherd.dev>
2020-05-27 21:27:09 -0600

    responsiveness

--- root/js/main.js
+++ root/js/main.js
@@ -7,39 +7,6 @@ let animation = anime({
   direction: 'alternate',
 });
 
-let animation2 = anime({
-  targets: '.animate-div1',
-  translateX: 200,
-  borderRadius: 200,
-  duration: 2000,
-  easing: 'linear',
-  direction: 'alternate',
-});
-let animation3 = anime({
-  targets: '.animate-div2',
-  translateX: -200,
-  borderRadius: 200,
-  duration: 2000,
-  easing: 'linear',
-  direction: 'alternate',
-});
-let animation4 = anime({
-  targets: '.animate-div3',
-  translateX: 200,
-  borderRadius: 200,
-  duration: 2000,
-  easing: 'linear',
-  direction: 'alternate',
-});
-let animation5 = anime({
-  targets: '.animate-div4',
-  translateX: -200,
-  borderRadius: 200,
-  duration: 2000,
-  easing: 'linear',
-  direction: 'alternate',
-});
-
 // MODAL STUFF //
 
 let modal = document.getElementById('myModal');
