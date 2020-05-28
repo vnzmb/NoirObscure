7c02873049a805e53ee0ff45b5bd7ae29ff0921a
Ian Shepherd <me@ianshepherd.dev>
2020-05-27 18:05:59 -0600

    some changes to sections

---
+++ root/js/main.js
@@ -0,0 +1,41 @@
+let animation = anime({
+  targets: '.content',
+  translateX: 100,
+  borderRadius: 200,
+  duration: 2000,
+  //   easing: 'linear',
+  direction: 'alternate',
+});
+
+let animation2 = anime({
+  targets: '.animate-div1',
+  translateX: 200,
+  borderRadius: 200,
+  duration: 2000,
+  easing: 'linear',
+  direction: 'alternate',
+});
+let animation3 = anime({
+  targets: '.animate-div2',
+  translateX: -200,
+  borderRadius: 200,
+  duration: 2000,
+  easing: 'linear',
+  direction: 'alternate',
+});
+let animation4 = anime({
+  targets: '.animate-div3',
+  translateX: 200,
+  borderRadius: 200,
+  duration: 2000,
+  easing: 'linear',
+  direction: 'alternate',
+});
+let animation5 = anime({
+  targets: '.animate-div4',
+  translateX: -200,
+  borderRadius: 200,
+  duration: 2000,
+  easing: 'linear',
+  direction: 'alternate',
+});
